2ce82fb282972f991735f038164891428a0f1424
Long1 <tuhulalimon@gmail.com>
2020-09-11 09:49:54 +0700

    Long

---
+++ ptrinhbacnhat/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ptrinhbacnhat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("nhap vao a: ");
+            float a = float.Parse(Console.ReadLine());
+
+            if (a == 0)
+            {
+                Console.WriteLine("a phai khac 0");
+            }
+            else
+            {
+                Console.WriteLine("nhap vao b: ");
+                float b = float.Parse(Console.ReadLine());
+
+                float x = -b / a;
+                Console.WriteLine("{0}x + {1} = 0 \n=> x = {2}", a, b, x);
+            }
+            Console.ReadKey();
+        }
+    }
+}
